eb1afe5f89d8673e6677236bded5929db6340254
Apb8 <alexiapala8@gmail.com>
2023-12-15 18:27:01 +0100

    Added new modules

---
+++ Game/Source/EnemyBee.cpp
@@ -0,0 +1,1 @@
+#include "EnemyBee.h"
---
+++ Game/Source/EnemyBee.h
@@ -0,0 +1,15 @@
+#ifndef __ENEMYBEE_H__
+#define __ENEMYBEE_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+
+class EnemyBee
+{
+
+};
+
+#endif // __ENEMYBEE_H__
