f55d67d6f000978e4815c0cac9a69e2be89b03de
Apb8 <alexiapala8@gmail.com>
2023-12-15 18:46:14 +0100

    Creation in process for bee module + sprites

--- Game/Source/EnemyBee.h
+++ Game/Source/EnemyBee.h
@@ -7,9 +7,37 @@
 
 struct SDL_Texture;
 
-class EnemyBee
+class EnemyBee : public Entity
 {
+public:
+	EnemyBee();
 
+	virtual ~EnemyBee();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update(float dt);
+	
+	bool CleanUp();
+
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+public:
+	
+	//Declaare enemy parameters
+	SDL_Texture* texture = NULL;
+
+	// Add physics to the enemy - declare a Physics body
+	float speed = 5.0f;
+	PhysBody* pbody;
+
+	pugi::xml_node config;
+	uint texW, texH;
+
+private:
+	//Por si de cara a futuro lo necesitamos
 };
 
 #endif // __ENEMYBEE_H__
